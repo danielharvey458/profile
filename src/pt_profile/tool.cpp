0e2897bff8493263f769531e31ba1e41ed235097
danielharvey458 <danielharvey458@gmail.com>
2019-05-15 20:45:45 +0100

    Configure from config file.

--- src/pt_profile/tool.cpp
+++ src/pt_profile/tool.cpp
@@ -6,17 +6,39 @@
 #include <sys/ptrace.h>
 #include <unistd.h>
 
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+std::vector<std::pair<std::intptr_t, std::intptr_t>>
+config_from_file (std::istream &&input)
+{
+  std::vector<std::pair<std::intptr_t, std::intptr_t>> result;
+  std::string temp;
+
+  while (std::getline (input, temp))
+  {
+    std::intptr_t start, stop;
+    std::stringstream string_stream (temp);
+
+    string_stream >> std::hex >> start >> stop;
+    result.emplace_back (start, stop);
+  }
+
+  return result;
+}
+
 int main (int argc, char **argv)
 {
   using namespace pt_profile;
 
-  if (argc != 4)
+  if (argc != 3)
   {
-    std::cerr << "Usage: " << argv[0] << " START END PROGRAM" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " CONFIG_FILE PROGRAM" << std::endl;
     return EXIT_FAILURE;
   }
 
-  const auto prog = argv[3];
+  const auto prog = argv[2];
   const auto pid = fork ();
 
   if (pid == 0)
@@ -27,8 +49,11 @@ int main (int argc, char **argv)
   else if (pid >= 1)
   {
     auto dbg = Debugger (prog, pid);
-    dbg.set_measure (std::strtol (argv[1], nullptr, 16),
-                     std::strtol (argv[2], nullptr, 16));
+
+    for (const auto &[start, stop] : config_from_file (std::ifstream (argv[1])))
+    {
+      dbg.set_measure (start, stop);
+    }
     dbg.run ();
   }
 }
