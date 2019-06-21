374d71bef69d40383820fe9fef529e6b8cb64aaf
danielharvey458 <danielharvey458@gmail.com>
2019-06-21 05:52:55 +0100

    Update 'find_function' test

--- tests/test_tool_regression/test_executables/find_function.cpp
+++ tests/test_tool_regression/test_executables/find_function.cpp
@@ -4,9 +4,27 @@ namespace nmsp
   {
     for (auto i = 0; i < 1 << 10; ++i) {}
   }
+
+  struct Foo
+  {
+    int bar ()
+    {
+      return 3;
+    }
+  };
+
+  template<typename T>
+  T template_function (const T &t)
+  {
+    return t;
+  }
 }
 
 int main ()
 {
   nmsp::namespaced ();
+  const auto lambda_function_thing = [] (int, long) {return 1;};
+  return nmsp::Foo ().bar ()
+   + lambda_function_thing (1, 2)
+   + nmsp::template_function (3.0);
 }
